cd8c547ce5703eafd31b18f37eac47df2126f2d5
Sergio Belda Galbis <serbelga@inf.upv.es>
2020-01-19 21:49:36 +0100

    Update project

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -10,19 +10,6 @@ import { HttpClient } from '@angular/common/http';
 export class ProjectsComponent implements OnInit {
   projects: any;
   projectsUrl = 'https://raw.githubusercontent.com/serbelga/serbelga.github.io/data/data/projects.json';
-  slides = [
-    {img: 'http://placehold.it/350x150/000000'},
-    {img: 'http://placehold.it/350x150/111111'},
-    {img: 'http://placehold.it/350x150/333333'},
-    {img: 'http://placehold.it/350x150/666666'},
-    {img: 'http://placehold.it/350x150/666666'},
-    {img: 'http://placehold.it/350x150/666666'},
-    {img: 'http://placehold.it/350x150/333333'},
-    {img: 'http://placehold.it/350x150/666666'},
-    {img: 'http://placehold.it/350x150/666666'},
-    {img: 'http://placehold.it/350x150/666666'}
-  ];
-  slideConfig = {slidesToShow: 4, slidesToScroll: 4};
   constructor(private sanitizer: DomSanitizer, private http: HttpClient) { }
   ngOnInit() {
     this.http.get(this.projectsUrl).subscribe(
